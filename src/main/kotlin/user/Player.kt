166317fa25af881d645bf19cd201cf8bdb6d2432
BuildTools <5360339+camdenorrb@users.noreply.github.com>
2019-06-25 16:01:08 -0500

    Remove Player

--- src/main/kotlin/user/Player.kt
+++
@@ -1,7 +0,0 @@
-package user
-
-import net.dv8tion.jda.core.entities.Game
-import net.dv8tion.jda.core.entities.Guild
-import net.dv8tion.jda.core.entities.User
-
-data class Player(val user: User, var game: Game, val mutualGuilds: MutableList<Guild> = mutableListOf())
